e3a8282a16f3382b8602e85fc6071857d95171b7
nolangh <nolanghiggins@gmail.com>
2021-07-29 12:17:32 -0600

    added constructor function

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-function NBAPlayer(name, team, position) {}
+// NOTE this is the constructor function for NBA player creation.
+function NBAPlayer(name, team, position) {
+  this.name = name;
+  this.team = team;
+  this.postion = position;
+}

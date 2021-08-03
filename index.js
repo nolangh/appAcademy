80682c2cc3b80681c2786d26f2c9b6b6da03a63b
nolangh <nolanghiggins@gmail.com>
2021-08-03 15:13:06 -0600

    re worked NBA player class constructor

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-/* --------------------------------- STEP 1 --------------------------------- */
-
-// ANCHOR make constructor function.
-function NBAPlayer(name, team, position) {
-  //"this. assigns & creats attributes"
-  this.name = name;
-  this.team = team;
-  this.postion = position;
+// Class Example.
+class NbaPlayer {
+  // you need a constructor function.
+  constructor(name, position, team) {
+    this.name = name;
+    this.position = position;
+    this.team = team;
+  }
 }
 
-/* --------------------------------- STEP 2 --------------------------------- */
-//ANCHOR make a player
-//use "new" to pass the new player through constructor function and assign attributs.
-let curry = new NBAPlayer("Steph Curry", "GS Warriors", "Point Gaurd");
-
-//using ".prototype" allows you to add to the constructor function.
-// we use '.prototype' to add dunks to the original constructor function. we also assigned a function to NBAPlayers.dunk which returns ('this.name' + dunks!)
-NBAPlayer.prototype.dunk = function () {
-  return `${this.name} dunks!`;
-};
-
-//will return the dunk function output that was assigned to NBAPlayer
-console.log(curry.dunk());
-
-/* NOTE
-the below code will log '{dunk:[function]}' that was assigned to NBAPLayer.
-NBAPlayer.prototype;
-console.log(NBAPlayer.prototype)
-*/
-
-/*to change a property calue of a player or object just re-assign it.
- curry.name = "Steph" will change his name from curry to steph
-*/
+let curry = new NbaPlayer("Steph Curry", "Point Gaurd", "GS Warriors");
+console.log(curry.team);
